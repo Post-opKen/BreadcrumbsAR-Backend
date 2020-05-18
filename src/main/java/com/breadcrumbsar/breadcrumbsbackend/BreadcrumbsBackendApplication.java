2b1621f6a24e1ba017e687ab36105e58cf703e58
Ean Daus <Eandaus123@gmail.com>
2020-05-18 16:15:22 -0700

    Initial commit

---
+++ src/main/java/com/breadcrumbsar/breadcrumbsbackend/BreadcrumbsBackendApplication.java
@@ -0,0 +1,15 @@
+package com.breadcrumbsar.breadcrumbsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadcrumbsBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BreadcrumbsBackendApplication.class, args);
+    }
+
+}
